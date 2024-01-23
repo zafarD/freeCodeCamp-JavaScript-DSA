82b3d71d3ab41f9818b919b6c1e62cd03cee00ba
zafarD <zafarahasan1703@gmail.com>
2024-01-23 14:40:03 +0530

    23.01

--- project-1-pallindrome-checker/script.js
+++ project-1-pallindrome-checker/script.js
@@ -1,11 +1,37 @@
 const textInput = document.getElementById("text-input");
-const checkButton = document.getElementById("check-button");
+const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
 function getInput() {
     const text = textInput.value;
-    console.log(text);
+    text ? "Ok" : window.alert("Please input a value");
+    return text;
 }
 
-checkButton.addEventListener("click", getInput);
+function isPalindrome(text) {
+    if (text.length === 1) return true;
+    for (let i = 0; i <= Math.floor(text.length / 2); i++) {
+        if (text[i] !== text[text.length - 1 - i]) {
+            return false;
+        }
+    }
+}
+function reset() {
+    result.innerHTML = "";
+}
+
+function showResult() {
+    const text = getInput();
+    const text2 = text.trim().toLowerCase();
+    const isTrue = isPalindrome(text2);
+    if (isTrue) {
+        result.innerHTML = `<h2>${text} is a Palindrome</h2>`
+    }
+    else {
+        result.innerHTML = `<h2>${text}  is not a palindrome</h2>`
+    }
+}
+
+
+checkButton.addEventListener('click', showResult);
 
